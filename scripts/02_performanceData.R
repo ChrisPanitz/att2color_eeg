5c9f2e8b5a0bd9f2ec084576ea08913ee82e48ca
Christian Panitz <christian.panitz@uni-leipzig.de>
2023-07-06 12:30:24 +0200

    Added performance measures

--- scripts/02_performanceData.R
+++ scripts/02_performanceData.R
@@ -2,7 +2,7 @@
 # --- encoding: en_US.UTF-8
 # --- R version: 4.0.3 (2020-10-10) -- "Bunny-Wunnies Freak Out"
 # --- RStudio version: 2022.12.0
-# --- script version: May 2023
+# --- script version: July 2023
 # --- content: summarize and plot performance data
 
 # load required packages
@@ -18,6 +18,14 @@ dfPerformance <- read.csv(filename, header = TRUE, sep = ";")
 describe(dfPerformance)
 
 # quick histograms for percentage of correct answers and number of trials without response
-hist(dfPerformance$percentCorrect, breaks = seq(0,100,5))
+hist(dfPerformance$percentCorrect, breaks = seq(0,100,10))
 hist(dfPerformance$nrRatingsTimedOut,
      breaks = -1:max(dfPerformance$nrRatingsTimedOut))
+
+# ... for percentage of misses (underestimates) in trials with only baseline targets and trials with only cued targets
+hist(dfPerformance$missesBL, breaks = seq(0,100,10))
+hist(dfPerformance$missesCue, breaks = seq(0,100,10))
+
+# ... for percentage of reporting zero targets in trials without targets and with at least one target
+hist(dfPerformance$repZeroTargetsAbsent, breaks = seq(0,100,10))
+hist(dfPerformance$repZeroTargetsPresent, breaks = seq(0,100,10))
